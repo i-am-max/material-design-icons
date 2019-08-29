c67a4b7ba679cbc25d7ed25f722fb723e837d0db
maxloh <35831069+maxloh@users.noreply.github.com>
2019-08-29 18:58:40 +0800

    Update build script 2

--- download.js
+++ download.js
@@ -1,32 +1,34 @@
 const fs = require('fs');
 const download = require('download');
-
-const json = JSON.parse(fs.readFileSync('./data.json'));
-const styles = ['baseline', 'outline', 'round', 'twotone', 'sharp'];
-const fileTypes = [
-    '24px.svg',
-    'black-18.zip', 'black-24.zip', 'black-36.zip', 'black-48.zip', 'ios-black.zip', 'android-black.zip',
-    'white-18.zip', 'white-24.zip', 'white-36.zip', 'white-48.zip', 'ios-white.zip', 'android-white.zip'
-];
-const downloadType = async (fileType) => {
-    let PromiseArray = []
-    for (let category of json.categories) {
-        for (let icon of category.icons) {
-            for (let style of styles) {
-                let url = `https://material.io/resources/icons/${json.baseUrl}${style}-${icon.id}-${fileType}`;
-                PromiseArray.push(
-                    new Promise(async (resolve) => {
-                        try { await download(url, `./src/${fileType}/${category.name}/${icon.id}`) } catch (e) { console.log(`Not downloaded: ${url}`) };
-                        resolve();
-                    })
-                );
-            }
-        }
-    }
-    return Promise.all(PromiseArray);
-}
+const fetch = require('node-fetch');
 
 (async () => {
+    const json = await (await fetch('https://material.io/tools/icons/static/data.json')).json();
+    const styles = ['baseline', 'outlined', 'round', 'twotone', 'sharp'];
+    const fileTypes = [
+        '24px.svg',
+        'black-18.zip', 'black-24.zip', 'black-36.zip', 'black-48.zip', 'ios-black.zip', 'android-black.zip',
+        'white-18.zip', 'white-24.zip', 'white-36.zip', 'white-48.zip', 'ios-white.zip', 'android-white.zip'
+    ];
+
+    const downloadType = async (fileType) => {
+        let PromiseArray = []
+        for (let category of json.categories) {
+            for (let icon of category.icons) {
+                for (let style of styles) {
+                    let url = `https://fonts.gstatic.com/s/i/materialicons${style !== 'baseline' ? style : ''}/${icon.id}/v1/${fileType}`;
+                    PromiseArray.push(
+                        new Promise(async (resolve) => {
+                            try { await download(url, `./src/${fileType}/${category.name}/${icon.id}`); } catch (e) { console.log(`Not downloaded: ${url}`); };
+                            resolve();
+                        })
+                    );
+                }
+            }
+        }
+        return Promise.all(PromiseArray);
+    }
+
     for (let fileType of fileTypes) {
         console.log(`Downloading: ${fileType}`);
         await downloadType(fileType);
