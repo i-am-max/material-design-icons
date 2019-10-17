e2f7e502ff0cb9418567436a81af9d799535447c
maxloh <35831069+maxloh@users.noreply.github.com>
2019-10-18 01:46:13 +0800

    Format code

--- download.js
+++ download.js
@@ -17,9 +17,9 @@ const fetch = require('node-fetch');
         for (let icon of json.icons) {
             for (let family of json.families) {
                 const url = `https://fonts.gstatic.com/s/i/${family.replace(/ /g, '').toLowerCase()}/${icon.name}/v1/${fileType}`;
+                const familyName = family.replace(/Material Icons/, '').replace(/ /g, '').toLowerCase();
                 PromiseArray.push(
                     new Promise(async (resolve) => {
-                        const familyName = family.replace(/Material Icons/, '').replace(/ /g, '').toLowerCase();
                         try { await download(url, `./src/${fileType}/${icon.categories[0]}/${icon.name}`, { filename: `${familyName !== '' ? `${familyName}-` : ''}${icon.name}-${fileType}` }); } catch (e) { console.log(`Not downloaded: ${url}`); };
                         resolve();
                     })
