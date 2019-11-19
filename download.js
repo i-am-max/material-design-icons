99ccbdb67b7a7d4ebd7fa55ea02a1c046e084268
maxloh <35831069+maxloh@users.noreply.github.com>
2019-11-19 23:05:21 +0800

    Improved logging

--- download.js
+++ download.js
@@ -9,11 +9,6 @@ import download from 'download';
         return JSON.parse(text.substring(text.indexOf("\n") + 1));
     })();
 
-    if (fs.existsSync('icons.json') && isequal(JSON.parse(fs.readFileSync('icons.json', 'utf8')), json)) return;
-    console.log('Deleting old src folder\n');
-    fs.rmdirSync('./src', { recursive: true });
-    fs.writeFileSync('icons.json', JSON.stringify(json));
-
     const fileTypes = [
         '24px.svg',
         'black-18dp.zip', 'black-24dp.zip', 'black-36dp.zip', 'black-48dp.zip', 'black-ios.zip', 'black-android.zip',
@@ -21,25 +16,48 @@ import download from 'download';
     ];
 
     const downloadType = async (fileType) => {
-        let PromiseArray = []
+        process.stdout.write(`Downloading: ${fileType}`);
+        let PromiseArray = [];
+        let allDownloaded = true;
         for (let icon of json.icons) {
             for (let family of json.families) {
                 const url = `https://fonts.gstatic.com/s/i/${family.replace(/ /g, '').toLowerCase()}/${icon.name}/v${icon.version}/${fileType}`;
                 const familyName = family.replace(/Material Icons/, '').replace(/ /g, '').toLowerCase();
                 PromiseArray.push(
                     new Promise(async (resolve) => {
-                        try { await download(url, `./src/${fileType}/${icon.categories[0]}/${icon.name}`, { filename: `${familyName + (familyName ? '-' : '')}${icon.name}-${fileType}` }); } catch (e) { console.log(`Not downloaded: ${url}`); };
+                        try {
+                            await download(url, `./src/${fileType}/${icon.categories[0]}/${icon.name}`, { filename: `${familyName + (familyName ? '-' : '')}${icon.name}-${fileType}` });
+                        } catch (e) {
+                            console.log(`Not downloaded: ${url}`);
+                            allDownloaded = false;
+                        };
                         resolve();
                     })
                 );
             }
         }
-        return Promise.all(PromiseArray);
+        await Promise.all(PromiseArray);
+        if (allDownloaded) {
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+        }
+        process.stdout.write(`Downloaded: ${fileType}\n`);
     }
 
-    for (let fileType of fileTypes) {
-        console.log(`Downloading: ${fileType}`);
-        await downloadType(fileType);
-        console.log(`Downloaded: ${fileType}`);
+    if (fs.existsSync('icons.json') && isequal(JSON.parse(fs.readFileSync('icons.json', 'utf8')), json)) {
+        console.log('No changes found in https://fonts.google.com/metadata/icons');
+        return;
     }
+
+    process.stdout.write(`Deleting old src folder`);
+    fs.rmdirSync('./src', { recursive: true });
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Deleted old src folder\n`);
+
+    for (let fileType of fileTypes) {
+        await downloadType(fileType);
+    }
+
+    fs.writeFileSync('icons.json', JSON.stringify(json));
 })();
