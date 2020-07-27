076020fa856d3ff2d2cd94b7a88d64bf4dafa1a6
maxloh <35831069+maxloh@users.noreply.github.com>
2020-07-27 19:14:22 +0800

    Also download old versions of icons and removed icons

--- download.js
+++ download.js
@@ -1,12 +1,31 @@
 import * as fs from 'fs';
 import fetch from 'node-fetch';
-import isequal from 'lodash.isequal';
+import opentype from 'opentype.js';
+import _ from 'lodash';
 import download from 'download';
 
 (async () => {
   const metadata = await (async () => {
     const text = await (await fetch('https://fonts.google.com/metadata/icons')).text();
     return JSON.parse(text.substring(text.indexOf("\n") + 1));
+  })();
+
+  // Get list of removed icons from font ligatures of MaterialIcons-Regular.ttf
+  const removedIcons = (() => {
+    const font = opentype.loadSync('./fonts/Material_Icons/MaterialIcons-Regular.ttf');
+    const invertedGlyphIndexMap = _.invert(font.tables.cmap.glyphIndexMap);
+    const ligatures = font.tables.gsub.lookups.flatMap(lookup =>
+      lookup.subtables.flatMap(subtable => {
+        const coverage = subtable.coverage.ranges.flatMap(range => _.range(range.start, range.end + 1).map(i => String.fromCharCode(parseInt(invertedGlyphIndexMap[i]))));
+        return subtable.ligatureSets.flatMap((ligatureSet, ligatureSetIndex) =>
+          ligatureSet.map(ligature => `${coverage[ligatureSetIndex]}${ligature.components.map(component => String.fromCharCode(parseInt(invertedGlyphIndexMap[component]))).join('')}`)
+        );
+      })
+    );
+    // Remove duplicates with Set constructor and spread syntax 
+    return [...new Set(ligatures)]
+      .filter(ligature => !metadata.icons.find(icon => icon.name === ligature))
+      .map(iconName => ({ name: iconName, version: 20, categories: ['removed'] })); // Start downloading from version 20, end at version 1
   })();
 
   const fileTypes = [
@@ -17,24 +36,27 @@ import download from 'download';
 
   const downloadFileType = async (fileType) => {
     let allDownloaded = true;
-    const PromiseArray = metadata.icons.flatMap(icon =>
+    const PromiseArray = [...removedIcons, ...metadata.icons].flatMap(icon =>
       metadata.families.map(family =>
         new Promise(async resolve => {
-          const familyName = family.replace(/Material Icons/, '').replace(/ /g, '').toLowerCase();
-          const url = `https://fonts.gstatic.com/s/i/materialicons${familyName}/${icon.name}/v${icon.version}/${fileType}`;
-          const retryLimit = 100;
-          let retryCount = 0;
-          while (retryCount < retryLimit) {
-            try {
-              await download(url, `./src/${fileType}/${icon.categories[0]}/${icon.name}`, { filename: `${familyName + (familyName ? '-' : '')}${icon.name}-${fileType}` });
-              break;
-            } catch (e) {
-              retryCount++;
+          for (let version = icon.version; version > 0; version--) {
+            const familyName = family.replace(/Material Icons/, '').replace(/ /g, '').toLowerCase();
+            const url = `https://fonts.gstatic.com/s/i/materialicons${familyName}/${icon.name}/v${version}/${fileType}`;
+            const retryLimit = 100;
+            let retryCount = 0;
+            while (retryCount < retryLimit) {
+              try {
+                await download(url, `./src/${fileType}/${icon.categories[0]}/${icon.name}/v${version}`, { filename: `${familyName + (familyName ? '-' : '')}${icon.name}-${fileType}` });
+                break;
+              } catch (error) {
+                if (error.statusCode === 404) break;
+                else retryCount++;
+              }
             }
-          }
-          if (retryCount === retryLimit) {
-            allDownloaded = false;
-            console.log(`Not downloaded: ${url}`);
+            if (retryCount === retryLimit) {
+              allDownloaded = false;
+              console.log(`Not downloaded: ${url}`);
+            }
           }
           resolve();
         })
@@ -49,16 +71,10 @@ import download from 'download';
     process.stdout.write(`Downloaded: ${fileType}\n`);
   };
 
-  if (fs.existsSync('icons.json') && isequal(JSON.parse(fs.readFileSync('icons.json', 'utf8')), metadata)) {
+  if (fs.existsSync('icons.json') && _.isEqual(JSON.parse(fs.readFileSync('icons.json', 'utf8')), metadata)) {
     console.log('No changes found in https://fonts.google.com/metadata/icons');
     return;
   }
-
-  process.stdout.write(`Deleting old src folder`);
-  fs.rmdirSync('./src', { recursive: true });
-  process.stdout.clearLine();
-  process.stdout.cursorTo(0);
-  process.stdout.write(`Deleted old src folder\n`);
 
   for (const fileType of fileTypes) {
     await downloadFileType(fileType);
